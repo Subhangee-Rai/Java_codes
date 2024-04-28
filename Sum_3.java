6b91586e5b6350b265ba8a4638f3f5649f748cfc
Subhangee <105161854+Subhangee-Rai@users.noreply.github.com>
2024-04-28 16:58:21 +0530

    Add files via upload

---
+++ Sum_3.java
@@ -0,0 +1,10 @@
+public class Sum_3 {
+    public static void main(String[] args) {
+        int num1=10,num2=20,num3=30;
+        System.out.println("Sum of integer numbers is: "+(num1+num2+num3));
+        float f1=10.1f, f2=10.2f;
+        double f3=10.3; //double can be declared without using d after the value while f is needed for float.
+        System.out.println("\nSum of floating numbers is: "+(f1+f2+f3));
+
+    }
+}
